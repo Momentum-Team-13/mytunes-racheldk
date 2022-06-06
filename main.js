8cccd81e87bf8afa846581ffe3d082aa52ea60bd
Rachel Kelly <racheldk126@gmail.com>
2022-06-06 14:46:49 -0700

    all search results populating on page

--- main.js
+++ main.js
@@ -1,45 +1,56 @@
-const search = document.getElementById('searchButton')
-const songCard = document.getElementsByClassName('songCard')
-const artwork = document.getElementById('artwork')
-const title = document.getElementById('trackName')
-const artist = document.getElementById('artistName')
-
+const search = document.getElementById("searchButton");
+let resultBox = document.querySelector(".resultBox");
+// console.log("songCard made");
 
 // submit.addEventListener("click", function (event){
-//     ///something here to make the result be part of the fetch?
-//     console.log("search clicked")
-//     //initiate whatever logic thing to replace spaces with plusses
-//     return term
-//     let term = searchBar.replaceAll(" ", "+")
-// })
-
-
-
-let url = "https://itunes.apple.com/search?term=nada+surf&entity=song"
-//possible way to make the fetch url? `url${searchBar.text}&entity=song?
-//also going to need a logic thing to replace the spaces with pluses
-fetch(url, {
-    method:"GET",
-    headers: {"Content-Type": 'application/JSON'},
-}).then(function (response){
-    return response.json()
-}) .then(function (data) {
-    console.log(data.results);
-    song(data.results)
-})
-
-function song(results) {
-    for (let song of results){
+    //     ///something here to make the result be part of the fetch?
+    //     console.log("search clicked")
+    //     //initiate whatever logic thing to replace spaces with plusses
+    //     return term
+    //     let term = searchBar.replaceAll(" ", "+")
+    // })
+    
+    let url = "https://itunes.apple.com/search?term=nada+surf&entity=song";
+    //possible way to make the fetch url? `url${searchBar.text}&entity=song?
+    //also going to need a logic thing to replace the spaces with pluses
+    fetch(url, {
+        method: "GET",
+        headers: { "Content-Type": "application/JSON" },
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.results);
+        song(data.results);
+    });
+    
+    function song(results) {
+        for (let song of results) {
+            resultBox.appendChild(addSong(song))
+        }
+    }
+    
+    function addSong(song) {
+    let songCard = document.createElement("div");
+    songCard.classList.add("songCard");
+    let artwork = document.createElement("img");
     artwork.src = song.artworkUrl100;
-    console.log("songCard.src");
+    artwork.alt = `${song.collectionName} album art`;
+    songCard.appendChild(artwork);
+    // console.log("artwork made");
+    
+    let title = document.createElement("div");
+    title.classList.add("title");
     title.innerText = `${song.trackName}`;
-    console.log(title.innerText);
-    artist.innerText = `${song.artistName}`;
-    console.log(artist.innerText);  
-}}
-
-
-
-
-// make sure you connect 
-//make an event listener on 
+    songCard.appendChild(title);
+    // console.log(title);
+    
+    let artist = document.createElement("div");
+    artist.classList.add("artist");
+    artist.innerText = song.artistName;
+    songCard.appendChild(artist);
+    // console.log(artist);
+    
+    return songCard
+}
