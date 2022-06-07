17eeaf026a45249592bd4f20e05119ebe932e07f
Rachel Kelly <racheldk126@gmail.com>
2022-06-07 08:51:28 -0700

    search bar working

--- README.md
+++ README.md
@@ -16,13 +16,14 @@ Here are the steps you'll need to take in order to complete this project.
 ## Product Requirements
 
 - A user can search for songs by artist or band name.
-- Validate the input so that a search request is not sent with no search term.
+x Validate the input so that a search request is not sent with no search term.
 - Display the search results without reloading the page.
-- The results should include song details. You must include the song title, but the data you'll get back from the API has other info you can consider including, like artist name, album title, album image, and release date.
-- The number of results you show is up to you. If no results are returned from a search, your UI should communicate this to the user in a clear way (how you do this is up to you).
-- You should handle responses from the server that are not in the 200 range in your javascript and also in your UI (for instance, you can show an error message and ask the user to repeat their search).
+x The results should include song details. You must include the song title, but the data you'll get back from the API has other info you can consider including, like artist name, album title, album image, and release date.
+- The number of results you show is up to you. 
+- If no results are returned from a search, your UI should communicate this to the user in a clear way (how you do this is up to you). (***check if there are 0 results and then display no results)
+??? - You should handle responses from the server that are not in the 200 range in your javascript and also in your UI (for instance, you can show an error message and ask the user to repeat their search).
 - Allow the user to click on a song title to play a song preview.
-- Your application should be nicely styled, with thought given to the user experience (that is, it should be easy for your user to interact with).
+x Your application should be nicely styled, with thought given to the user experience (that is, it should be easy for your user to interact with).
 
 ### Setup with `npm`
 
--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 const search = document.getElementById("searchButton");
 let resultBox = document.querySelector(".resultBox");
+const form = document.querySelector("#myForm")
 // console.log("songCard made");
+const inputField = document.querySelector("#searchBar")
 
-// submit.addEventListener("click", function (event){
-    //     ///something here to make the result be part of the fetch?
-    //     console.log("search clicked")
-    //     //initiate whatever logic thing to replace spaces with plusses
-    //     return term
-    //     let term = searchBar.replaceAll(" ", "+")
-    // })
+//TRYING TO DO THE FORM THING 
+form.addEventListener( "submit", function (event){
+    //submit  handles submission by click and by enter (and any other accessibility tools used)
     
-    let url = "https://itunes.apple.com/search?term=nada+surf&entity=song";
-    //possible way to make the fetch url? `url${searchBar.text}&entity=song?
-    //also going to need a logic thing to replace the spaces with pluses
-    fetch(url, {
-        method: "GET",
-        headers: { "Content-Type": "application/JSON" },
-    })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data.results);
-        song(data.results);
+        event.preventDefault();
+        //what does preventDefault do? default behavior is for the page to reload, which we don't want because the results will go away until we ask them to with a new search
+        getResults(inputField.value)
+        console.log(inputField.value)
     });
-    
-    function song(results) {
+
+    function getResults(term){
+        let url = `https://itunes.apple.com/search?term=${term}&entity=song`;
+        //possible way to make the fetch url? `url${searchBar.text}&entity=song?
+        //also going to need a logic thing to replace the spaces with pluses
+        fetch(url, {
+            method: "GET",
+            headers: { "Content-Type": "application/JSON" },
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.results);
+            song(data.results);
+        });
+        
+    }
+        function song(results) {
         for (let song of results) {
             resultBox.appendChild(addSong(song))
         }
@@ -51,6 +57,12 @@ let resultBox = document.querySelector(".resultBox");
     // album.innerText = `${song.collectionName}`;
     // songCard.appendChild(album);
     // console.log(album)
+
+    let audio = document.createElement('audio')
+    audio.classList.add('audio')
+    audio.src = song.previewUrl
+    songCard.appendChild(audio)
+    // console.log("audio file")
     
     let artist = document.createElement("div");
     artist.classList.add("artist");
@@ -60,3 +72,4 @@ let resultBox = document.querySelector(".resultBox");
     
     return songCard
 }
+
