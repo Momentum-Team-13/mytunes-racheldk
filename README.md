6195ecdec87660dad0260e4fdcc6ec0cba3fcc0a
Rachel Kelly <racheldk126@gmail.com>
2022-06-08 13:42:42 -0700

    cleanup

--- README.md
+++ README.md
@@ -20,9 +20,9 @@ x Validate the input so that a search request is not sent with no search term.
 x Display the search results without reloading the page.
 x The results should include song details. You must include the song title, but the data you'll get back from the API has other info you can consider including, like artist name, album title, album image, and release date.
 x The number of results you show is up to you. 
-- If no results are returned from a search, your UI should communicate this to the user in a clear way (how you do this is up to you). (***check if there are 0 results and then display no results)
-??? - You should handle responses from the server that are not in the 200 range in your javascript and also in your UI (for instance, you can show an error message and ask the user to repeat their search).
-- Allow the user to click on a song title to play a song preview.
+X If no results are returned from a search, your UI should communicate this to the user in a clear way (how you do this is up to you). (***check if there are 0 results and then display no results)
+X - You should handle responses from the server that are not in the 200 range in your javascript and also in your UI (for instance, you can show an error message and ask the user to repeat their search).
+X Allow the user to click on a song title to play a song preview.
 x Your application should be nicely styled, with thought given to the user experience (that is, it should be easy for your user to interact with).
 
 ### Setup with `npm`
--- main.js
+++ main.js
@@ -1,103 +1,93 @@
 const search = document.getElementById("searchButton");
 let resultBox = document.querySelector(".resultBox");
 const form = document.querySelector("#myForm");
-// console.log("songCard made");
 const inputField = document.querySelector("#searchBar");
 const player = document.querySelector("#audioPlayer");
 const now = document.querySelector("#now");
 
 form.addEventListener("submit", function (event) {
-  //submit  handles submission by click and by enter (and any other accessibility tools used)
-  resultBox.innerHTML = "";
-
-  event.preventDefault();
-  //what does preventDefault do? default behavior is for the page to reload, which we don't want because the results will go away until we ask them to with a new search
-  getResults(inputField.value);
-  console.log(inputField.value);
+    resultBox.innerHTML = "";
+    event.preventDefault();
+    getResults(inputField.value);
+    console.log(inputField.value);
 });
 
 function getResults(term) {
-  let url = `https://itunes.apple.com/search?term=${term.replaceAll(
-    " ",
-    "+"
-  )}&entity=song`;
-  console.log(url);
-  fetch(url, {
-    method: "GET",
-    headers: { "Content-Type": "application/JSON" },
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("status code error: " + response.status);
-      }
+    let url = `https://itunes.apple.com/search?term=${term.replaceAll(
+        " ",
+        "+"
+    )}&entity=song`;
+    console.log(url);
+    fetch(url, {
+        method: "GET",
+        headers: { "Content-Type": "application/JSON" },
     })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("status code error: " + response.status);
+            }
+        })
 
-    .then(function (data) {
-      console.log(data.results);
-      song(data.results);
-      if (data.results.length === 0) {
-        console.log("no results found");
-        let noResults = document.createElement("div");
-        noResults.classList.add("noResults");
-        noResults.innerText = `No results found`;
-        resultBox.appendChild(noResults);
-      }
-    });
+        .then(function (data) {
+            console.log(data.results);
+            song(data.results);
+            if (data.results.length === 0) {
+                console.log("no results found");
+                let noResults = document.createElement("div");
+                noResults.classList.add("noResults");
+                noResults.innerText = `No results found`;
+                resultBox.appendChild(noResults);
+            }
+        });
 }
 function song(results) {
-  for (let song of results) {
-    resultBox.appendChild(addSong(song));
-  }
+    for (let song of results) {
+        resultBox.appendChild(addSong(song));
+    }
 }
 
 function addSong(song) {
-  let songCard = document.createElement("div");
-  songCard.classList.add("songCard");
-  let artwork = document.createElement("img");
-  artwork.classList.add("artwork");
-  artwork.src = song.artworkUrl100;
-  artwork.alt = `${song.collectionName} album art`;
-  songCard.appendChild(artwork);
-  // console.log("artwork made");
+    let songCard = document.createElement("div");
+    songCard.classList.add("songCard");
+    let artwork = document.createElement("img");
+    artwork.classList.add("artwork");
+    artwork.src = song.artworkUrl100;
+    artwork.alt = `${song.collectionName} album art`;
+    songCard.appendChild(artwork);
 
-  let title = document.createElement("div");
-  title.classList.add("title");
-  title.innerText = `${song.trackName}`;
-  songCard.appendChild(title);
-  // console.log(title);
+    let title = document.createElement("div");
+    title.classList.add("title");
+    title.innerText = `${song.trackName}`;
+    songCard.appendChild(title);
 
-  let audioSource = document.createElement("div");
-  audioSource.classList.add("audio");
-  audioSource.innerText = `${song.previewUrl}`;
-  songCard.appendChild(audioSource);
-  // console.log("audio file")
+    let audioSource = document.createElement("div");
+    audioSource.classList.add("audio");
+    audioSource.innerText = `${song.previewUrl}`;
+    songCard.appendChild(audioSource);
 
-  let artist = document.createElement("div");
-  artist.classList.add("artist");
-  artist.innerText = song.artistName;
-  songCard.appendChild(artist);
-  // console.log(artist);
+    let artist = document.createElement("div");
+    artist.classList.add("artist");
+    artist.innerText = song.artistName;
+    songCard.appendChild(artist);
 
-  return songCard;
+    return songCard;
 }
 
 resultBox.addEventListener("click", function (event) {
-  let target = event.target;
-  console.log(target);
-  console.log(target.parentElement);
-  if (target.parentElement.classList.contains("songCard")) {
-    console.log(target.parentElement.children);
-    player.src = target.parentElement.children[2].innerText;
-    now.innerText = `Now Playing: ${target.parentElement.children[1].innerText} by ${target.parentElement.children[3].innerText}`;
-    console.log("now playing updated");
-  } else if (target.classList.contains("songCard")) {
-    console.log("target.classList");
-    player.src = target.children[2].innerText;
-    now.innerText = `Now Playing: ${target.children[1].innerText} by ${target.children[3].innerText}`;
-    console.log("now playing updated");
-  }
+    let target = event.target;
+    console.log(target);
+    console.log(target.parentElement);
+    if (target.parentElement.classList.contains("songCard")) {
+        console.log(target.parentElement.children);
+        player.src = target.parentElement.children[2].innerText;
+        now.innerText = `Now Playing: ${target.parentElement.children[1].innerText} by ${target.parentElement.children[3].innerText}`;
+        console.log("now playing updated");
+    } else if (target.classList.contains("songCard")) {
+        console.log("target.classList");
+        player.src = target.children[2].innerText;
+        now.innerText = `Now Playing: ${target.children[1].innerText} by ${target.children[3].innerText}`;
+        console.log("now playing updated");
+    }
 });
-
-//~~~fix formatting so the long song titles don't mess stuff up
